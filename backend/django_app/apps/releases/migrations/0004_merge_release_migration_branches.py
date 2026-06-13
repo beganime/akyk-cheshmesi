28a77a7a3e848bd7eea7bd514df90ff65629a391
beganime <begenchyagmurow2008@gmail.com>
2026-06-13 12:12:48 +0500

    Merge release migration branches

---
+++ backend/django_app/apps/releases/migrations/0004_merge_release_migration_branches.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("releases", "0002_rename_app_release_version_idx_app_release_version_c1ef12_idx_and_more"),
+        ("releases", "0003_package_file_label"),
+    ]
+
+    operations = []
